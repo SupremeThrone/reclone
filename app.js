3bf493eb31a418e123219f09ee16b46cb935e39a
Tom <archnor@gmail.com>
2018-04-11 16:54:38 +0100

    Set up bodyParser and serve static files

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const express = require("express");
+const express = require("express"),
+      bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/views"));
 
 app.get("/", (req, res) => {
     res.send("Hello");
