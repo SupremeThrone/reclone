56e87fdd54701f1e72020361ec41749213be0e6f
Tom <archnor@gmail.com>
2018-04-11 19:52:21 +0100

    add pug and display on index page

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 const express = require("express"),
       bodyParser = require("body-parser"),
       Post = require("./models/posts"),
-      db = require("./models");
+      db = require("./models"),
+      pug = require('pug'),
+      path = require("path")
 
 const app = express();
-
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "pug");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/views"));
 
+//API Route. Used for JS.
+app.get("/post/api", (req, res) => {
+    db.Post.find()
+     .then(function(posts){
+         res.json(posts);
+     })
+     .catch(function(err){
+         res.send(err);
+     })
+  });
+
+
 app.get("/", (req, res) => {
-   Post.find()
-    .then(function(posts){
-        res.json(posts);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
- });
+    db.Post.find()
+     .then(function(posts){
+         res.render("index", {posts: posts})
+     })
+      .catch(function(err){
+          console.log(err);
+      })
+});
+
+
 
  app.post("/post/new", (req, res) =>{
      Post.create(req.body)
--- models/index.js
+++ models/index.js
@@ -3,3 +3,6 @@ mongoose.set("debug", true);
 mongoose.connect("mongodb://localhost/reclone");
 
 mongoose.Promise = Promise;
+
+//Get the posts schema for Promises functionality(?)
+module.exports.Post = require("./posts")
